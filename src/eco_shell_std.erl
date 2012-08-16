8531e2a61bb090115fb35df6a7ece89042889058
Adam Rutkowski <hq@mtod.org>
2012-08-16 12:10:01 +0200

    Make standard commands module use the names() API

--- src/eco_shell_std.erl
+++ src/eco_shell_std.erl
@@ -1,9 +1,19 @@
 -module(eco_shell_std).
 -export([reload/1]).
+-export([hi/1]).
 
 reload(help) ->
-    <<"Reload configuration.">>;
+    {reply, <<"Reload configuration.">>};
+
 reload({completions, 1}) ->
-    ["abc", "foo", "bar"];
+    [ binary_to_list(X) || X <- eco:names() ];
+
 reload([Filename]) ->
-    eco:reload(Filename).
+    eco:reload(Filename);
+reload([_, _]) ->
+    erlang:error(wussup).
+
+hi([Name]) ->
+    io_lib:format("hi ~s", [Name]);
+hi({completions, 1}) ->
+    ["adam"].
